55f02c34d1619563daf255020f95db1313449b81
aidenjones29 <36414445+aidenjones29@users.noreply.github.com>
2020-10-08 16:57:40 +0100

    wad
    awd

---
+++ Dice/Assets/Scripts/ProjectileFire.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileFire : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject projectile;
+    [SerializeField]
+    private float MaxFireCooldown = 1;
+    [SerializeField]
+    private int projectileSpeed = 500;
+
+    private float currFireCooldown = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currFireCooldown = MaxFireCooldown;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("RTrigger") > 0)
+        {
+            if (currFireCooldown <= 0)
+            {
+                Quaternion playerRot = Quaternion.identity;
+                playerRot.eulerAngles = new Vector3(0, transform.eulerAngles.y, 90);
+
+                GameObject bullet = Instantiate(projectile, transform.position, playerRot) as GameObject;
+                bullet.GetComponent<Rigidbody>().AddForce(transform.forward * projectileSpeed);
+
+                currFireCooldown = MaxFireCooldown;
+            }
+        }
+        currFireCooldown -= Time.deltaTime;
+    }
+}
---
+++ Dice/Assets/Scripts/ProjectileTimer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileTimer : MonoBehaviour
+{
+    [SerializeField]
+    private float MaxTimer = 4;
+    private float curTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        curTimer = MaxTimer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(curTimer > 0)
+        {
+            curTimer -= Time.deltaTime;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
