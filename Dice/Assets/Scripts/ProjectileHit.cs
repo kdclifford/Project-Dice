d1ce959943a022f306c1ad8c8add026433331bed
aidenjones29 <36414445+aidenjones29@users.noreply.github.com>
2020-10-18 00:48:13 +0100

    awd
    awd

---
+++ Dice/Assets/Scripts/ProjectileHit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileHit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollision(Collision collision)
+    {
+        Destroy(this.gameObject);
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        Destroy(this.gameObject);
+    }
+}
