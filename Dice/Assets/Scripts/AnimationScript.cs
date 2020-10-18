661295327bf028b92afd1d0007855bd25cdcafa2
kdclifford <47176557+kdclifford@users.noreply.github.com>
2020-10-18 17:21:39 +0100

    more animations

--- Dice/Assets/Scripts/AnimationScript.cs
+++ Dice/Assets/Scripts/AnimationScript.cs
@@ -9,27 +9,25 @@ namespace AnimationFunctions.Utils
     public class AnimationScript
     {
         // public bool rightFire = false;
-        public static void Idle4(Animator animator)
-        {
-
-
-            int idle = Random.Range(0, 4);
-
-            switch (idle)
-            {
-                case 0:
-                    animator.SetInteger("Animation", 0);
-                    break;
-                case 1:
-                    animator.SetInteger("Animation", 7);
-                    break;
-                case 2:
-                    animator.SetInteger("Animation", 8);
-                    break;
-                case 3:
-                    animator.SetInteger("Animation", 9);
-                    break;
-            }
+        public static void Idle(Animator animator)
+        {            
+                int idle = Random.Range(0, 4);
+            
+                switch (idle)
+                {
+                    case 0:
+                        animator.SetInteger("Animation", 0);
+                        break;
+                    case 1:
+                        animator.SetInteger("Animation", 7);
+                        break;
+                    case 2:
+                        animator.SetInteger("Animation", 8);
+                        break;
+                    case 3:
+                        animator.SetInteger("Animation", 9);
+                        break;
+                }            
         }
 
         public static void RightAttack(Animator animator)
