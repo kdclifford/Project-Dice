b35814e822a8286711349b8e92b301234de08fc2
kdclifford <47176557+kdclifford@users.noreply.github.com>
2020-10-19 19:01:55 +0100

    more collision stuff

--- Dice/Assets/Scripts/MovementScript.cs
+++ Dice/Assets/Scripts/MovementScript.cs
@@ -19,7 +19,15 @@ public class MovementScript : MonoBehaviour
    
     public bool walking = false;
 
-    Vector3 rayOffset = new Vector3(0, 0.5f, 0);
+    public Vector3 rayOffset = new Vector3(0, 1.5f, 0);
+
+    Vector3 upRight = Vector3.forward + Vector3.right;
+    Vector3 upLeft = Vector3.forward + Vector3.left;
+    Vector3 downRight = Vector3.back + Vector3.right;
+    Vector3 downLeft = Vector3.back + Vector3.left;
+
+    public float diagonalOffset = 0.9f;
+    public float collisionForce = 1.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -104,7 +112,44 @@ public class MovementScript : MonoBehaviour
         //}
         // if (Physics.SphereCast(transform.position, 2, new Vector3(horizontalInput, 1, verticalInput), out hit, rayDist))
 
-        
+
+        if (UpRight() && DownRight())
+        {
+            if (Physics.Raycast(transform.position + rayOffset, Vector3.right, out hit, rayDist, ~layerMask))
+            {
+                //Vector3 newPos = new Vector3(transform.position.x - (rayDist - hit.distance), transform.position.y, transform.position.z);
+                //transform.position = newPos;
+
+                GetComponent<Rigidbody>().AddForce(Vector3.left * collisionForce);                
+            }
+        }
+
+        if (DownLeft() && UpLeft())
+        {
+            if (Physics.Raycast(transform.position + rayOffset, Vector3.left, out hit, rayDist, ~layerMask))
+            {
+                GetComponent<Rigidbody>().AddForce(Vector3.right * collisionForce);
+            }
+        }
+
+        if (DownRight() && DownLeft())
+        {
+            if (Physics.Raycast(transform.position + rayOffset, Vector3.down, out hit, rayDist, ~layerMask))
+            {
+                GetComponent<Rigidbody>().AddForce(Vector3.forward * collisionForce);
+            }
+        }
+
+        if (UpLeft() && UpRight())
+        {
+            if (Physics.Raycast(transform.position + rayOffset, Vector3.forward, out hit, rayDist, ~layerMask))
+            {
+                GetComponent<Rigidbody>().AddForce(Vector3.back * collisionForce);
+            }
+        }
+
+
+
 
         if (Physics.Raycast(transform.position + rayOffset, Vector3.forward, out hit, rayDist, ~layerMask))
         {
@@ -112,6 +157,11 @@ public class MovementScript : MonoBehaviour
             {
                 verticalInput = 0;
             }
+            Debug.DrawRay(transform.position, Vector3.forward * rayDist, Color.red);
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, Vector3.forward * rayDist, Color.green);
         }
 
 
@@ -121,6 +171,11 @@ public class MovementScript : MonoBehaviour
             {
                 verticalInput = 0;
             }
+            Debug.DrawRay(transform.position, Vector3.back * rayDist, Color.red);
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, Vector3.back * rayDist, Color.green);
         }
 
 
@@ -130,6 +185,11 @@ public class MovementScript : MonoBehaviour
             {
                 horizontalInput = 0;
             }
+            Debug.DrawRay(transform.position, Vector3.right * rayDist, Color.red);
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, Vector3.right * rayDist, Color.green);
         }
 
 
@@ -145,8 +205,13 @@ public class MovementScript : MonoBehaviour
                 horizontalInput = 0;
             }
 
-
+            Debug.DrawRay(transform.position, Vector3.left * rayDist, Color.red);
         }
+        else
+        {
+            Debug.DrawRay(transform.position, Vector3.left * rayDist, Color.green);
+        }
+
         
 
             transform.Translate(new Vector3(horizontalInput, 0, verticalInput) * moveSpeed * Time.deltaTime, Space.World);
@@ -163,6 +228,77 @@ public class MovementScript : MonoBehaviour
             GetComponent<Rigidbody>().velocity = Vector3.zero;
         }
     }
+
+
+    bool UpLeft()
+    {
+        if (Physics.Raycast(transform.position + rayOffset, upLeft, rayDist * diagonalOffset, ~layerMask))
+        {
+
+            //horizontalInput = -horizontalInput;
+            //verticalInput = -verticalInput;
+
+            //if (Mathf.Abs(verticalInput) > Mathf.Abs(horizontalInput))
+            //{
+            //    verticalInput = 0;
+            //}
+            //else if (Mathf.Abs(verticalInput) < Mathf.Abs(horizontalInput))
+            //{
+            //    horizontalInput = 0;
+            //}
+            Debug.DrawRay(transform.position, upLeft * (rayDist * diagonalOffset), Color.red);
+            return true;
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, upLeft * (rayDist * diagonalOffset), Color.green);
+            return false;
+        }
+    }
+
+
+    bool UpRight()
+    {
+        if (Physics.Raycast(transform.position + rayOffset, upRight, rayDist * diagonalOffset, ~layerMask))
+        {            
+            Debug.DrawRay(transform.position, upRight * (rayDist * diagonalOffset), Color.red);
+            return true;
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, upRight * (rayDist * diagonalOffset), Color.green);
+            return false;
+        }
+    }
+
+    bool DownRight()
+    {
+        if (Physics.Raycast(transform.position + rayOffset, downRight, rayDist * diagonalOffset, ~layerMask))
+        {
+            Debug.DrawRay(transform.position, downRight * (rayDist * diagonalOffset), Color.red);
+            return true;
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, downRight * (rayDist * diagonalOffset), Color.green);
+            return false;
+        }
+    }
+
+    bool DownLeft()
+    {
+        if (Physics.Raycast(transform.position + rayOffset, downLeft, rayDist * diagonalOffset, ~layerMask))
+        {
+            Debug.DrawRay(transform.position, downLeft * (rayDist * diagonalOffset), Color.red);
+            return true;
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, downLeft * (rayDist * diagonalOffset), Color.green);
+            return false;
+        }
+    }
+
 
 
 }
