4ea4f6c091e1c08e73ce60e559b9554775499600
David-Reddington <57140418+David-Reddington@users.noreply.github.com>
2020-11-18 16:48:05 +0000

    Revert "Revert "Merge branch 'Merging-Branch' into main""
    This reverts commit 25f16282d0463ef9bb4add99a9b8d8b59537eb54.

---
+++ Dice/Assets/Scripts/PCG/Room.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room :MonoBehaviour
+{
+    public List<int> connectedRooms;
+    public Vector2 Size;
+    public List<Vector2Int> DoorLocations = new List<Vector2Int>();
+    public List<FacingDirection> DoorFacingDirections = new List<FacingDirection>();
+    public RoomType roomType;
+    public Vector2Int location; 
+    public int preFabNumber;
+    public int roomNumber;
+    // Start is called before the first frame update
+
+}
