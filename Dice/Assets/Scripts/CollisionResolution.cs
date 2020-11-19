43f75d3da633faad95f0b6bee29e1fa75ad8bbed
kdclifford <47176557+kdclifford@users.noreply.github.com>
2020-11-19 16:35:51 +0000

    EnemyController

--- Dice/Assets/Scripts/CollisionResolution.cs
+++ Dice/Assets/Scripts/CollisionResolution.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hit : MonoBehaviour
+public class CollisionResolution : MonoBehaviour
 {
     private GameObject textPrefab;
     private SoundManager soundManager;
--- Dice/Assets/Scripts/EnemyController.cs
+++ Dice/Assets/Scripts/EnemyController.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 using AnimationFunctions.Utils;
 using UnityEngine.AI;
 
-public class EnemyAi : MonoBehaviour
+public class EnemyController : MonoBehaviour
 {
     //Public
+    [SerializeField, Header("Object Referneces")]
+    private GameObject projectile;
+    [SerializeField]
+    private RandomColour meshRenderer;
     public GameObject target;   
     public bool isDead = false;
     private float health;
@@ -16,12 +20,28 @@ public class EnemyAi : MonoBehaviour
     private NavMeshAgent agent;
     private Animator animator;
 
+    [SerializeField, Header("Projectile offsets"), Tooltip("Controls how high the projectile spawns")]
+    private float yOffsetProgectile = 1;
+    [SerializeField, Tooltip("Controls how far forward the projectile spawns")]
+    private float zOffsetProgectile = 1;
+    [SerializeField, Header("Projectile Settings")]
+    private float fireRate = 1;
+    [SerializeField]
+    private float projectileSpeed = 10;
+
+    private float fireCooldown = 0;
+    private int playerHealth;
+    private SoundManager soundManager;
+
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
         agent.avoidancePriority = 0;
+        fireCooldown = fireRate;
+        animator = GetComponent<Animator>();
+        soundManager = GameObject.FindGameObjectWithTag("SoundManager").GetComponent<SoundManager>();
     }
 
     // Update is called once per frame
@@ -64,9 +84,7 @@ public class EnemyAi : MonoBehaviour
                 // transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 100 * Time.deltaTime);
                 transform.LookAt(target.transform);
 
-                GetComponent<EnemyFire>().EnemyShoot();
-
-
+                EnemyShoot();
             }
         }
 
@@ -75,13 +93,75 @@ public class EnemyAi : MonoBehaviour
             float posY = transform.position.y - (removeSpeed * Time.deltaTime);
             transform.position = new Vector3(transform.position.x, posY, transform.position.z);
         }
-
+        fireCooldown -= Time.deltaTime;
     }
 
     public void RemoveBody()
     {
         removeBody = true;
         Destroy(gameObject, 5);
+    }  
+
+    //Universal call to make enemies
+    public void EnemyShoot()
+    {
+        if (projectile.tag != ("NotEquipped"))
+        {
+            if (playerHealth >= 0 && fireCooldown <= 0)
+            {
+                AnimationScript.SpiderAttack(animator);
+            }
+            else
+            {
+                AnimationScript.StopAttack(animator);
+            }
+        }
     }
 
+    //Spider Attack
+    public void Fire()
+    {
+        if (GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>() != null)
+        {
+            SpawnBullet(0, 0);
+            SpawnBullet(10, 45);
+            SpawnBullet(-10, -45);
+        }
+    }
+
+    //Wisard attacks
+    public void WizardFire()
+    {
+        if (GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>() != null)
+        {
+            SpawnBullet(0, 0);
+        }
+    }
+
+    //Changes the staff colour while shooting
+    public void StaffAttack()
+    {
+        meshRenderer.lerpOn = true;
+    }
+
+    //Spawns projectiles
+    void SpawnBullet(float angleMin, float angleMAx)
+    {
+        Vector3 forward = transform.forward;
+        forward.y = 0;
+
+        Quaternion playerRot = Quaternion.identity;
+        playerRot.eulerAngles = new Vector3(0, transform.eulerAngles.y + Random.Range(angleMin, angleMAx), 90);
+
+        Vector3 firePos = transform.position;
+        firePos.y += yOffsetProgectile;
+        firePos += transform.forward * zOffsetProgectile;
+
+        //playerRot.eulerAngles += 45;
+        GameObject bullet = Instantiate(projectile, firePos, playerRot) as GameObject;
+        bullet.GetComponent<Rigidbody>().AddForce(bullet.transform.forward * projectileSpeed);
+        bullet.tag = "EnemyProjectile";
+        soundManager.Play(projectile.name, bullet);
+        fireCooldown = fireRate;
+    }
 }
