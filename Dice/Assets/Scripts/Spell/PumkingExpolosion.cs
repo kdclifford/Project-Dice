8f000c83f95e3c5c4df94ff4543863a0c7eece0b
David-Reddington <57140418+David-Reddington@users.noreply.github.com>
2020-12-06 14:32:04 +0000

    Started Mushroom Spell
    Found a pumkin asset to use isntead for now

---
+++ Dice/Assets/Scripts/Spell/PumkingExpolosion.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PumkingExpolosion : SpellBase
+{
+    // Start is called before the first frame update
+    public override void CastSpell(Vector3 posistion, Quaternion playerRot, string tag = "Equipped")
+    {
+
+        //Summon A Pumkin 
+
+        Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, viewRadious, targetMask);
+    }
+    public override void SetValues()
+    {
+        SpellObject = Resources.Load("Player/Electric", typeof(GameObject)) as GameObject;
+        UILogo = Resources.Load<Sprite>("UIIcons/Spells/LightningUI");
+    }
+}
