0c594d116cd5b950ae01a2fd9dfe9830aa77266f
aidenjones29 <36414445+aidenjones29@users.noreply.github.com>
2020-12-28 15:51:55 +0000

    spells & AOE damage
    awd

--- Dice/Assets/Scripts/Spell/AOE Spells/AOEDamage.cs
+++ Dice/Assets/Scripts/Spell/AOE Spells/AOEDamage.cs
@@ -6,18 +6,40 @@ public class AOEDamage : MonoBehaviour
 {
     private Collider SpellCollider;
 
+    [SerializeField] private float Duration;
     [SerializeField] private float tickTimeMax;
     private float CurrTickTime;
 
     // Start is called before the first frame update
     void Start()
     {
-        CurrTickTime = tickTimeMax;
+        SpellCollider = gameObject.GetComponent<Collider>();
+        SpellCollider.enabled = false;
+        CurrTickTime = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
         CurrTickTime -= Time.deltaTime;
+        Duration -= Time.deltaTime;
+
+        if (CurrTickTime <= 0.0f)
+        {
+            StartCoroutine(CollisionTimer());
+            CurrTickTime = tickTimeMax;
+        }
+
+        if(Duration <= 0.0f)
+        {
+            Object.Destroy(gameObject);
+        }
+    }
+
+    IEnumerator CollisionTimer()
+    {
+        SpellCollider.enabled = true;
+        yield return new WaitForSeconds(0.2f);
+        SpellCollider.enabled = false;
     }
 }
---
+++ Dice/Assets/Scripts/Spell/AOE Spells/SummonMeteor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SummonMeteor : SpellBase
+{
+    public override void CastSpell(Vector3 posistion, float rot, GameObject agentRef, string tag = "Equipped")
+    {
+        SetValues();
+        //Summon A Pumkin 
+        BasicSummon(posistion, rot, tag, SpellObject, ESpellEnum.SummonMeteor);
+    }
+    public override void SetValues()
+    {
+        SpellObject = Resources.Load(PathProjectile + "SummonMeteor", typeof(GameObject)) as GameObject;
+        UILogo = Resources.Load<Sprite>(PathUI + "RockUI");
+        destroyInstantiante = Resources.Load(PathDeath + "EmptyDestroy", typeof(GameObject)) as GameObject;
+    }
+
+    public SummonMeteor()
+    {
+        duration = 10;
+        castingColour = Color.red;
+        element = EElementalyType.Rock;
+        castingSound = ESoundClipEnum.Bubble;
+        spellType = ESpellType.AOE;
+        deathSound = ESoundClipEnum.Bubble;
+        SpellName = "DinoKiller";
+    }
+}
