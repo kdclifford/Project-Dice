e71299e4cfe279c0f653316c08a61af5343c2c2c
David-Reddington <57140418+David-Reddington@users.noreply.github.com>
2021-02-20 14:07:22 +0000

    Update FireWork.cs

--- Dice/Assets/Scripts/Spell/Fire/FireWork.cs
+++ Dice/Assets/Scripts/Spell/Fire/FireWork.cs
@@ -10,20 +10,19 @@ public class FireWork : SpellBase
     public override void CastSpell(Vector3 posistion, float rot, GameObject agentRef, string tag = "Equipped")
     {
        GameObject effect = MonoBehaviour.Instantiate(particlePrefab, posistion, Quaternion.identity) as GameObject;
-        MonoBehaviour.Destroy(effect, 1.5f);
-        //Spell Code would go in here then when controller has correct input Cast Spell
-        for (int i = 0; i < 10; i++)
-        {
-
-            Quaternion agentRot = Quaternion.identity;
-            agentRot.eulerAngles = new Vector3(0, rot + (36 * i), 90);
-            GameObject bullet = MonoBehaviour.Instantiate(SpellObject, posistion, agentRot) as GameObject;
-            bullet.GetComponent<Rigidbody>().AddForce(bullet.transform.forward * projectileSpeed);
-            bullet.tag = tag;
-            bullet.GetComponent<ProjectileController>().setTimer(duration);
-            bullet.AddComponent<SpellIndex>().spellIndex = ESpellEnum.FireWork;
-            SoundManager.instance.Play(castingSound, bullet);
-        }
+       MonoBehaviour.Destroy(effect, 1.5f);
+       //Spell Code would go in here then when controller has correct input Cast Spell
+       for (int i = 0; i < 10; i++)
+       {
+           Quaternion agentRot = Quaternion.identity;
+           agentRot.eulerAngles = new Vector3(0, rot + (36 * i), 90);
+           GameObject bullet = MonoBehaviour.Instantiate(SpellObject, posistion, agentRot) as GameObject;
+           bullet.GetComponent<Rigidbody>().AddForce(bullet.transform.forward * projectileSpeed);
+           bullet.tag = tag;
+           bullet.GetComponent<ProjectileController>().setTimer(duration);
+           bullet.AddComponent<SpellIndex>().spellIndex = ESpellEnum.FireWork;
+           SoundManager.instance.Play(castingSound, bullet);
+       }
 
     }
 
