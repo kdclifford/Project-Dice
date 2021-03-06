a904a0a461a354e1446922f6f0ef0a84c6b73636
David-Reddington <57140418+David-Reddington@users.noreply.github.com>
2021-03-06 13:56:33 +0000

    Cheasts V1
    Basic Cheasts Impleemnted
    Hit boxes current issue
    PowerPickUp is needs to be rotated

---
+++ Dice/Assets/Scripts/World/CDungeonCheast.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CDungeonCheast : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject pivot;
+    public GameObject spellPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void openTheCheast()
+    {
+        pivot.transform.rotation = new Quaternion(-90,0 , 0, 0);
+        //Vector3 pivotPoint = pivot.transform.position;
+        //this.transform.RotateAround(pivotPoint, Vector3.down, -90);
+        gameObject.tag = "Wall";
+        Vector3 spawnPosition = transform.position;
+        spawnPosition.x -= 5;
+        Instantiate(spellPrefab, spawnPosition, Quaternion.identity);
+    }
+}
