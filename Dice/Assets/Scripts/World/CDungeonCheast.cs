4fe16deaff08e4d1b1099ec04c0daf469396f608
David-Reddington <57140418+David-Reddington@users.noreply.github.com>
2021-03-08 14:47:07 +0000

    Cheast Rotation

--- Dice/Assets/Scripts/World/CDungeonCheast.cs
+++ Dice/Assets/Scripts/World/CDungeonCheast.cs
@@ -8,6 +8,7 @@ public class CDungeonCheast : MonoBehaviour
     [SerializeField]
     private GameObject pivot;
     public GameObject spellPrefab;
+    public FacingDirection facingDirection;
 
     // Start is called before the first frame update
     void Start()
@@ -18,12 +19,32 @@ public class CDungeonCheast : MonoBehaviour
     // Update is called once per frame
     public void openTheCheast()
     {
-        pivot.transform.rotation = new Quaternion(-90,0 , 0, 0);
+        pivot.transform.rotation =  Quaternion.Euler(-90,0 , 0);
         //Vector3 pivotPoint = pivot.transform.position;
         //this.transform.RotateAround(pivotPoint, Vector3.down, -90);
         gameObject.tag = "Wall";
         Vector3 spawnPosition = transform.position;
-        spawnPosition.x -= 5;
-        Instantiate(spellPrefab, spawnPosition, Quaternion.identity);
+        if(facingDirection == FacingDirection.Up)
+        {
+            spawnPosition.z -= 5;
+        } 
+        else if (facingDirection == FacingDirection.Right)
+        {
+            spawnPosition.x += 5;
+
+        }
+        else if(facingDirection == FacingDirection.Down)
+        {
+            spawnPosition.z += 5;
+
+        }
+        else if(facingDirection == FacingDirection.Left)
+        {
+            spawnPosition.x -=5;
+
+        }
+        var quat = new Quaternion();
+        quat = Quaternion.Euler(90, 0, 0);
+        Instantiate(spellPrefab, spawnPosition, quat);
     }
 }
