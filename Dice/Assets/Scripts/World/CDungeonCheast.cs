61af799b13b3ed0d4504368dde65d80c037b11cf
aidenjones29 <36414445+aidenjones29@users.noreply.github.com>
2021-03-11 12:25:33 +0000

    chest fix
    awd

--- Dice/Assets/Scripts/World/CDungeonCheast.cs
+++ Dice/Assets/Scripts/World/CDungeonCheast.cs
@@ -19,11 +19,11 @@ public class CDungeonCheast : MonoBehaviour
     // Update is called once per frame
     public void openTheCheast()
     {
-        pivot.transform.rotation =  Quaternion.Euler(-90,0 , 0);
-        //Vector3 pivotPoint = pivot.transform.position;
-        //this.transform.RotateAround(pivotPoint, Vector3.down, -90);
+        pivot.transform.localRotation = Quaternion.Euler(-90, 0,0);
         gameObject.tag = "Wall";
+
         Vector3 spawnPosition = transform.position;
+
         if(facingDirection == FacingDirection.Up)
         {
             spawnPosition.z -= 5;
